5fd5d9bcd798395546fc1ae5e9657f0148a502c8
jjmonte20 <jjmonte20@gmail.com>
2018-11-02 14:24:39 -0500

    set parameters to make an article appear in the console

--- js/requestHandler.js
+++ js/requestHandler.js
@@ -4,23 +4,33 @@
 var apiKey = "d92e54e488034f8c917c32b1c09096b6"
 
 //have search be the value of what the user searched
-var search = $("#search").val();
+// var search = $("#term").val();
 
-//have filter be user input, preferably between one and ten results
-var filter = $("#filter").val();
+// //have filter be user input, preferably between one and ten results
+// var filter = $("#filter").val();
 
-//have starting date be the a date value
-var earlyDate = $("#earlyDate").val() + "0101";
+// //have starting date be the a date value
+// var earlyDate = $("#earlyDate").val() + "0101";
 
-//ending date is similar but a later date
-var endDate = $("#endDate").val() + "1231";
+// //ending date is similar but a later date
+// var endDate = $("#endDate").val() + "1231";
 
 $("#search").on("click", function() {
   console.log("click works");
-//query url is the combination of these fields plus the api search and API key
-var queryURL = "https://api.nytimes.com/svc/search/v2/?q=" + search + "&fq="
+var search = $("#term").val();
+
+// //have filter be user input, preferably between one and ten results
+var filter = $("#records").val();
+
+// //have starting date be the a date value
+var earlyDate = $("#startYear").val() + "0101";
+
+// //ending date is similar but a later date
+var endDate = $("#end-year").val() + "1231";
+// //query url is the combination of these fields plus the api search and API key
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + search + "&fq="
     + filter + "&begin_date=" + earlyDate + "&end_date=" + endDate +
-    "&api_key=" + apiKey;
+    "&api-key=" + apiKey;
 
     //now time to call an ajax function
     $.ajax({
@@ -34,5 +44,17 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/?q=" + search + "&fq="
         
 });
 })
-
+// Built by LucyBot. www.lucybot.com
+// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// url += '?' + $.param({
+//   'api-key': "d92e54e488034f8c917c32b1c09096b6"
+// });
+// $.ajax({
+//   url: url,
+//   method: 'GET',
+// }).done(function(result) {
+//   console.log(result);
+// }).fail(function(err) {
+//   throw err;
+// });
 
