43846efd256a04eb31746f8dbe401ee8893a781a
greenman <caasilemroh@gmail.com>
2018-11-02 12:42:20 -0500

    Finished the constructor for the articles. Also made a placeholder image

--- js/displayHandler.js
+++ js/displayHandler.js
@@ -1,15 +1,45 @@
+var articles = [];
 
-
-var article = function(){
+var ArticleNYT = function(){
 	var args = Array.prototype.slice.call(arguments);
 	
-	this.articleTitle = args[0] || "Placeholder title";
-	this.credit = args[1] || "Placeholder credit";
-	this.date = args[2] || (new Date()).getDate();
+	this.container = document.createElement("div");
+	this.container.classList.add("articleContainer");
+	
+	this.imageContainer = document.createElement("div");
+	this.imageContainer.classList.add("articleImageContainer");
 	
 	this.img = document.createElement("img");
-	this.img.src = args[3] || "../images/placeholder.png";
+	this.img.classList.add("articleImage");
+	this.img.src = args[0] || "../images/placeholder.png";
 	
+	this.imageContainer.appendChild(img);
 	
+	this.textContainer = document.createElement("div");
+	this.textContainer.classList.add("articleTextContainer");
+	
+	this.articleTitle = document.createElement("h3");
+	this.articleTitle.classList.add("articleTitle");
+	this.articleTitle.textContent = args[1] || "Placeholder title";
+	this.articleCredit = document.createElement("p");
+	this.articleCredit.classList.add("articleCredit");
+	this.articleCredit.textContent = args[2] || "Placeholder credit";
+	this.articleDate = document.createElement("p");
+	this.articleDate.classList.add("articleDate");
+	this.articleDate.textContent = args[3] || (new Date()).getDate();
+	this.articleSource = document.createElement("a");
+	this.articleSource.classList.add("articleTitle");
+	this.articleSource.href = args[4] || "#";
+	
+	this.textContainer.appendChild(articleTitle);
+	this.textContainer.appendChild(articleCredit);
+	this.textContainer.appendChild(articleDate);
+	this.textContainer.appendChild(articleSource);
+	
+	this.container.appendChild();
+	
+	this.getProduct = function(){
+		return this.container;
+	}
 	
 }
